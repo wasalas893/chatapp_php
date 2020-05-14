5e6fe62930f1864340be42dc89496be879f6e142
wasalas <wasalas893@gmail.com>
2020-05-14 21:39:35 +0530

    create the sign in css

--- signin.php
+++ signin.php
@@ -11,7 +11,9 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/signin.css">
     <title>chatapp</title>
+
 
 </head>
 <body>
@@ -44,7 +46,7 @@
            <!--  <?php //include("signin_user.php"); ?> -->
          
          </form>
-          <div class="text-center small" style="color: #674288;">Don't have an account?<a href="signup.php">Create one</a></div>
+          <div class="text-center small" style="color: red;">Don't have an account?<a href="signup.php">Create one</a></div>
       </div>
     
 </body>
