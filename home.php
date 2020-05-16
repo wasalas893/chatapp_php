b5b5980309427dc402122b1768395caa14a7d9b6
wasalas <wasalas893@gmail.com>
2020-05-16 13:05:59 +0530

    create the home.php and database

--- home.php
+++ home.php
@@ -1,1 +1,75 @@
-<h2>welcome to home page</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>chatapp-home</title>
+
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+ 
+</head>
+<body>
+    <div class="container main-section">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-12 left-sidebar">
+              <div class="input-group searchbox">
+                <div class="input-group-btn">
+                    <center><a href="include/find_friends.php"><button class="btn btn-default search-icon"
+                    name="search_user" type="submit">
+                    Add new user
+
+                    </button></a></center>
+                </div>
+             </div> 
+             <div class="left-chat">
+                 <ul>
+                     <?php include("include/get_users_data.php"); ?>
+                 </ul>
+             </div>
+
+            </div>
+
+            <div class="col-md-9 col-sm-9 col-xs-12 right-sidebar">
+                <div class="row">
+                    <!--getting the user information who is logged in -->
+                    <?php
+                    $user=$_SESSION['user_email'];
+                    $get_user="select * from users where user_email='$user'";
+                    $run_user=mysqli_query($con,$get_user);
+                    $row=mysqli_fetch_array($run_user);
+
+                    $user_id=$row['user_id'];
+                    $user_name=$row['user_name'];
+
+                    ?>
+
+                    <!--getting the user data on which user click-->
+                    <?php
+                         if(isset($GET['user_name'])){
+                             global $con;
+
+                             $get_username=$_GET['user_name'];
+                             $get_user="select * from users where user_name='$get_username'";
+
+                             $run_user=mysqli_query($con,$get_user);
+                             $row_user=mysqli_fetch_array($run_user);
+
+                             $username=$row_user['user_name'];
+                             $user_profile_image=$row_user['user_profile'];
+                         }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    
+</body>
+</html>
