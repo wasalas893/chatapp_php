ee4eab31cb6c57cd521e78aed479c43aeac7b7e4
wasalas <wasalas893@gmail.com>
2020-05-16 11:24:32 +0530

    create the signup_user.php and databese connect

---
+++ include/connection.php
@@ -0,0 +1,5 @@
+<?php
+ $con=mysqli_connect("localhost","root","","mychat") or die("Connection was not established");
+
+
+?>
--- signup.php
+++ signup.php
@@ -72,7 +72,7 @@
              <div class="form-group">
                  <button type="submit" class="btn btn-primary btn-block btn-lg" name="sign_up">Sign Up</button>
              </div>
-           <!--  <?php //include("signup_user.php"); ?> -->
+           <?php include("signup_user.php"); ?>
          
          </form>
           <div class="text-center small" style="color: red;">Already have an account?<a href="signin.php">Signin here</a></div>
