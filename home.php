71e579ba47284a0c41122ad67abf664d2d471e00
wasalas <wasalas893@gmail.com>
2020-05-16 15:28:30 +0530

    create the home.php and database

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
- 
+    <link rel="stylesheet" type="text/css" href="css/home.css">
 </head>
 <body>
     <div class="container main-section">
@@ -63,13 +63,133 @@
                              $username=$row_user['user_name'];
                              $user_profile_image=$row_user['user_profile'];
                          }
+
+                         $total_messages="select * from users_chat where (sender_username='$user_name'
+                         AND receiver_username='$username') OR (receiver_username='$user_name'
+                         AND sender_username='$username')";
+
+                         $run_messages=mysqli_query($con,$total_messages);
+                         $total=mysqli_num_rows($run_messages);
                     ?>
+
+                      <div class="col-md-12 right-header">
+                          <div class="right-header-img">
+                              <img src="<?php echo"$user_profile_image"; ?>">
+                          </div>
+                          <div class="right-header-detail">
+                              <form method="post">
+                                  <p><?php echo "$username";  ?></p>
+                                  <span><?php echo $total;  ?>messages</span>&nbsp &nbsp
+                                  <button name="logout" class="btn btn-danger">Logout</button>
+                              </form>
+                               <?php
+                               if(isset($_POST['logout'])){
+                                   $update_msg=mysqli_query($con,"UPDATE users SET log_in='offline'
+                                   WHERE user_name='$user_name'");
+                                   header("Location:logout.php");
+                                   exit();
+                               }
+
+                               ?>
+                          </div>
+
+                      </div>
+                </div>
+                <div class="row">
+                    <div id="scrolling_to_bottom" class="col-md-12 right-header-contentChat">
+                         <?php
+                             $update_msg=mysqli_query($con,"UPDATE users_chat SET msg_status='read'
+                             WHERE sender_username='$username' AND receiver_username='$user_name'");
+
+                             $sel_msg="select * from users_chat where (sender_username='$user_name'
+                             AND receiver_username='$username') OR (receiver_username='$user_name'
+                             AND sender_username='$username') ORDER by 1 ASC";
+
+                            $run_msg=mysqli_query($con,$sel_msg);
+                            
+                            while($row=mysqli_fetch_array($run_msg)){
+                                $sender_username=$row['sender_username'];
+                                $receiver_username=$row['receiver_username'];
+                                $msg_content=$row['msg_content'];
+                                $msg_date=$row['msg_date'];
+                            
+
+                          ?>
+                          <ul>
+                              <?php
+                                 if($user_name==$sender_username AND $username==$receiver_username){
+                                     echo"
+                                       <li>
+                                        <div class='rightside-chat'>
+                                          <span>$username<small>$msg_date</small></span>
+                                          <p>$msg_content</p>
+                                        </div>
+                                       
+                                       </li>
+                                       
+                                     ";
+                                 }
+                                 else if($user_name==$receiver_username AND $username==$sender_username){
+                                    echo"
+                                      <li>
+                                       <div class='rightside-chat'>
+                                         <span>$username<small>$msg_date</small></span>
+                                         <p>$msg_content</p>
+                                       </div>
+                                      
+                                      </li>
+                                      
+                                    ";
+                                }
+
+                              ?>
+                          </ul>
+                          <?php 
+                          
+                            }
+                          ?>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 right-chat-textbox">
+                        <form method="post">
+                            <input autocomplete="off" type="text" name="msg_content" placeholder="Write your messages......">
+                            <button class="btn" name="submit"><i class="fa fa-telegram" aria-hidden="true"></i></button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 
 
-    
+    <?php
+        if(isset($_POST['submit'])){
+            $msg=htmlentities(($_POST['msg_content']));
+
+            if($msg==""){
+                echo "
+                 <div class='alert alert-danger'>
+                 <strong><center>Message was unable to send</center></strong>
+                 </div>
+                
+                ";
+            }else if(strlen($msg)>100){
+                echo "
+                <div class='alert alert-danger'>
+                <strong><center>Message is Too long.Use only 100 characters</center></strong>
+                </div>
+               
+               ";
+
+            }else{
+                $insert="insert into users_chat(sender_username,receiver_username,msg_content
+                ,msg_status,msg_date) values('$user_name','$username','$msg','unread',NOW())";
+
+                $run_insert=mysqli_query($con,$insert);
+            }
+        }
+
+?>
 </body>
 </html>
